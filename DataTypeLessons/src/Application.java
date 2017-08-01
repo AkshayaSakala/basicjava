91ab92dd152489108ebfbc23b32723a9c3698b12
Akshaya Sakala <sakalaakshaya@gmail.com>
2017-08-01 12:05:24 -0700

    fourth session notes

---
+++ DataTypeLessons/src/Application.java
@@ -0,0 +1,13 @@
+
+public class Application {
+	public static void main(String[] args) {
+		
+		int x=2;
+		double y=2.5;
+		String name="Akshaya";
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(name);
+		}
+}
